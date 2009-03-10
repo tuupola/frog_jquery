793ac66998742f1c10d2e3b402c598c448916113
Mika Tuupola <tuupola@appelsiini.net>
2009-03-10 11:18:13 +0200

    jQuery 1.3.2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /*
  * jQuery - Add jQuery to Frog CMS admin interface.
  *
- * Copyright (c) 2008 Mika Tuupola
+ * Copyright (c) 2008-2009 Mika Tuupola
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -17,7 +17,7 @@ Plugin::setInfos(array(
     'id'          => 'jquery',
     'title'       => 'jQuery', 
     'description' => 'Add jQuery to admin interface.', 
-    'version'     => '1.3.0',
+    'version'     => '1.3.2',
     'license'     => 'MIT',
     'require_frog_version' => '0.9.3',
     'update_url'  => 'http://www.appelsiini.net/download/frog-plugins.xml',
