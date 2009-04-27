8ace24efabe2b6e7e5b1ad8072ab90fc99b4038a
Mika Tuupola <tuupola@appelsiini.net>
2009-04-27 17:08:19 +0300

    Prevent direct access to enable and disable scripts.
    Although it usually fails because of missing librarires malicous user can still access enable and disable PHP script via direct URL:
    
    http://www.example.com/frog/plugins/jquery/enable.php
    http://www.example.com/frog/plugins/jquery/disable.php

--- enable.php
+++ enable.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * jQuery - Add jQuery to Frog CMS admin interface.
+ *
+ * Copyright (c) 2008-2009 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   http://www.appelsiini.net/
+ *
+ */
+
+/* Prevent direct access. */
+if (!defined("FRAMEWORK_STARTING_MICROTIME")) {
+    die("All your base are belong to us!");
+}
+
 /* Force jQuery to be the first plugin. */
 unset(Plugin::$plugins['jquery']);
 Plugin::$plugins = array('jquery' => 1) + Plugin::$plugins;
